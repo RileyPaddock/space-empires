16a71024a5c3ab62d3e4726a04f175a66f2698ec
RileyPaddock <rileyrpaddock@gmail.com>
2021-01-11 06:32:16 +0000

    cleanup

--- src/units/unit.py
+++ src/units/unit.py
@@ -15,15 +15,3 @@ class Unit:
         unit_state['team']  =self.team
         return unit_state
 
-    def move(self,direction):
-        if direction == 'up':
-            self.location = (self.location[0],self.location[1]+1)
-        elif direction == 'down':
-            self.location = (self.location[0],self.location[1]-1)
-        elif direction == 'right':
-            self.location = (self.location[0]+1,self.location[1])
-        elif direction == 'left':
-            self.location = (self.location[0]-1,self.location[1])
-        else:
-            print("\n   You done messed up movement!")
-
